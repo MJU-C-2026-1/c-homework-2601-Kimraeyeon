844da1341e33532147996f0d209c23970a15f7f5
Kimraeyeon <jamppo109@mju.ac.kr>
2026-04-04 08:27:07 +0000

    60231879-김래연-과제1차완료

--- main.c
+++ main.c
@@ -9,7 +9,43 @@
 
 int main()
 {
-  printf("hello world");
+  //1. 변수 선언
+  char group;
+  int pay_hr;
+  int hours;
+  float tax_rate;
+  int pre_tax;
+  double final_pay;
+
+  //2. 정보 입력받기
+  printf("근무 조를 입력하세요(주간D / 야간 N) : ");
+  scanf(" %c", &group);
+
+  printf("시급을 입력하세요(원) : ");
+  scanf("%d",&pay_hr);
+
+  printf("이번달 일한 시간을 입력하세요(시간) : ");
+  scanf("%d", &hours);
+
+  printf("적용할 세율을 입력(예:3.3%%면 0.033) : ");
+  scanf("%f", &tax_rate);
+  
+  //3. 알바비 계산 (산술연산 사용)
+  pre_tax = pay_hr*hours ;
+  final_pay = pre_tax-(pre_tax*tax_rate);
+
+  //4. 영수증 형태의 결과 출력 
+  printf("\n=================================\n");
+  printf("[알바비 급여 명세서]\n");
+  printf("==================================\n");
+  printf("근무 조 : %c조\n", group);
+  printf("시급 : %d원\n", pay_hr);
+  printf("근무 시간 : %d시간\n",hours);
+  printf("적용 세율 : %.1f%%\n", tax_rate*100);
+  printf("----------------------------------\n");
+  printf("세전 총 급여 : %d원\n", pre_tax);
+  printf("세후 실 수령액 : %.1f원\n", final_pay);
+  printf("==================================\n");
 
   return 0; 
 }
